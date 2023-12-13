e637b824b690d97b51bc776ce038911eff894735
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-13 18:01:24 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <signal.h>
+
 /* Function prototypes */
 char *read_line(void);
 char **split_line(char *line);
