50e0c4f7625c966d193110d9a998924dc343646a
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-13 18:42:45 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -5,21 +5,29 @@ void execute(char **argv) {
     return; // Empty command
   }
 
-  value = getenv("PATH");
+  // Get PATH environment variable
+  char *value = getenv("PATH");
+
+  // Build linked list of paths in PATH
   head = linkpath(value);
-  pathname = _which(argv[0], head);
+
+  // Find the executable path
+  char *pathname = _which(argv[0], head);
 
   if (!pathname) {
     printf("Error: '%s' command not found\n", argv[0]);
     return;
   }
 
+  // Fork and execute the command
   pid_t pid = fork();
 
   if (pid == 0) {
+    // Child process
     execve(pathname, argv, environ);
     exit(1); // If execve fails
   } else {
+    // Parent process
     waitpid(pid, NULL, 0);
   }
 }
