0f5d6db542d55f47a68c09352a080d125ea753a9
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-13 17:46:26 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shell.h"
+
+
+/* Main function */
+int main(void)
+{
+    char *line; /* Buffer for the input line */
+    char **args; /* Array of arguments */
+    int status; /* Status of the execution */
+
+    do {
+        printf("> "); /* Display the prompt */
+        line = read_line(); /* Read the input line */
+        args = split_line(line); /* Split the line into arguments */
+        status = execute(args); /* Execute the arguments */
+
+        free(line); /* Free the allocated memory */
+        free(args);
+    } while (status); /* Loop until status is zero */
+
+    return 0;
+}
