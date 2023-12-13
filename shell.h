3787ee4e762a68de7c2efb637b19fcba260805cc
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-13 21:03:04 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
 /*
  * File: shell.h
  *
