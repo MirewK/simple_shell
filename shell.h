83115b37bb3849756d0289689286427d781895fa
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-15 18:53:43 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,28 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
-#define END_OF_FILE -2
-#define EXIT -3
 
-/*
- * File: shell.h
- *
- * Desc: Header file containing prototypes for all functions
- */
-/* Function prototypes */
-char *read_line(void);
-char **split_line(char *line);
-int launch(char **args);
-int execute(char **args);
-
-#endif
+#endif /* SHELL_H */
