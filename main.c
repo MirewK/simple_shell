45a69fb66502af469a300fe83e9684af9bcae0a0
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-15 19:22:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,70 +1,26 @@
 #include "shell.h"
 
 /**
-  * main - Getline function
-  * @argc: Argument count
-  * @argv: Array of argument values
+  * main - Wait program
+  * @argc: Argumnt count
+  * @argv: Array of arguments
+  * @env: Environment variable
   *
-  * Return: 0 on success
+  * Return: O Always succes
   */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[], char **env)
 {
-        (void)argc, (void)argv;
-        char *buf = NULL, *token;
-        size_t count = 0;
-        ssize_t nread;
-        pid_t child_pid;
-        int i, status;
-        char **array;
+	char *input_buffer;
+	(void)argc;
 
-        while (1)
-        {
-                write(STDOUT_FILENO, "SimpleShell$ ", 9);
+	while (1)
+	{
+		print_prompt();
+		input_buffer = read_input();
+		execute_command(input_buffer, argv, env);
+		free(input_buffer);
+	}
 
-                nread = getline(&buf, &count, stdin);
-
-                if (nread ==  -1)
-                {
-                        perror("Exiting shell");
-                        exit(1);
-                }
-
-                token = strtok(buf, " \n");
-
-                array = malloc(sizeof(char*) * 1024);
-                i = 0;
-
-                while (token)
-                {
-                        array[i] = token;
-                        token = strtok(NULL, " \n");
-                        i++;
-                }
-
-                array[i] = NULL;
-
-                child_pid = fork();
-
-                if (child_pid == -1)
-                {
-                        perror("Failed to create.");
-                        exit (41);
-                }
-
-                if (child_pid == 0)
-                {
-                        if (execve(array[0], array, NULL) == -1)
-                        {
-                                perror("Failed to execute");
-                                exit(97);
-                        }
-                }
-                else
-                {
-                        wait(&status);
-                }
-        }
-        free(buf);
-        return (0);
+	return (0);
 }
