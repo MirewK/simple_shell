a63c53193486cef6e83a970e3829d56c04fd6a96
robelinline <62613359+robelinline@users.noreply.github.com>
2023-12-15 19:08:08 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -8,5 +8,13 @@
 #include <sys/wait.h>
 #include <string.h>
 
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+pid_t fork(void);
+pid_t wait(int *wstatus);
+int execve(const char *pathname, char *const argv[],char *const envp[]);
+char *strtok(char *str, const char *delim);
+
+
+
 
 #endif /* SHELL_H */
